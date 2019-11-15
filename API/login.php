17ad5b33468daaf07baedb52b46b56bfc596f9aa
BraulioR-SaavedraE <braulio@ciencias.unam.mx>
2019-11-14 20:13:23 -0600

    Agregando login.php

---
+++ API/login.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__."/db/database.php";
+require __DIR__."/utilities/assert.php";
+
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+$user = $_GET["username"];
+$pass = $_GET["password"];
+$consulta="SELECT * FROM user WHERE username='$user' AND password='$pass' ";
+$db= Database::getInstance();
+$resultado=[];
+
+	try{
+		$rs = (object)$db->consulta($consulta);
+		$identificador=$user.$pass;
+		$resultado = [
+			"status" => "ok",
+			"key" => hash("sha256", $identificador),
+			];
+	}catch(Exception $e) {
+		$resultado = [
+			"status" => "failed",
+			"Message" => $e->getMessage(),
+			];
+	}
+	echo json_encode($resultado);
+}
+?>
